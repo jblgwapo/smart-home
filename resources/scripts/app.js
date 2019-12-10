fd6d4bd3c5aed5d1177b843b68cdf3ed929dd545
jblgwapo <jblgwapo@users.noreply.github.com>
2019-12-10 16:33:17 +0800

    service worker fix

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   console.log('ServiceWorker..');
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('service-worker.js')
     .then(function(registration) {
       console.log(
         'Service Worker registration successful with scope: ',
